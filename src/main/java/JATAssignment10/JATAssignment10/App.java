5461802a5c2334601f369965555be6a2951cfae8
pavanik26 <pavani.kashana26@gmail.com>
2024-11-01 15:37:31 +0530

    initial changes

---
+++ src/main/java/JATAssignment10/JATAssignment10/App.java
@@ -0,0 +1,10 @@
+package JATAssignment10.JATAssignment10;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
